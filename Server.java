8d2a12c414810337023b7afe6fba206856a1b234
조민성 <128134087+minseong204@users.noreply.github.com>
2023-11-25 01:20:54 +0900

    Create Server.java

---
+++ Server.java
@@ -0,0 +1,46 @@
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+
+    private ServerSocket serverSocket;
+
+    public Server(ServerSocket serversocket) {
+        this.serverSocket = serversocket;
+    }
+
+    public void startServer() {
+
+        try {
+
+            while (!serverSocket.isClosed()) {
+
+                Socket socket = serverSocket.accept();
+                System.out.println("새로운 클라이언트가 연결되었습니다.");
+                ClientHandler clientHandler = new ClientHandler(socket);
+
+                Thread thread = new Thread(clientHandler);
+                thread.start();
+            }
+        } catch (IOException e) {
+
+        }
+    }
+
+    public void closeServerSocket() {
+        try {
+            if (serverSocket != null) {
+                serverSocket.close();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        ServerSocket serverSocket = new ServerSocket(1234);
+        Server server = new Server(serverSocket);
+        server.startServer();
+    }
+}
