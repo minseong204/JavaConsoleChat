05453bae9730166ba9c56fcc852201de87efd64d
조민성 <128134087+minseong204@users.noreply.github.com>
2023-11-26 19:37:37 +0900

    Update Server.java

--- Server.java
+++ Server.java
@@ -1,30 +1,32 @@
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Server {
 
+    private static final Logger logger = LoggerFactory.getLogger(Server.class);
     private ServerSocket serverSocket;
+    private MongoDBUtil mongoDBUtil;
 
     public Server(ServerSocket serversocket) {
         this.serverSocket = serversocket;
+        this.mongoDBUtil = new MongoDBUtil();
     }
 
     public void startServer() {
-
         try {
-
             while (!serverSocket.isClosed()) {
-
                 Socket socket = serverSocket.accept();
-                System.out.println("새로운 클라이언트가 연결되었습니다.");
-                ClientHandler clientHandler = new ClientHandler(socket);
-
+                logger.info("새로운 클라이언트가 연결되었습니다.");
+                ClientHandler clientHandler = new ClientHandler(socket, mongoDBUtil);
                 Thread thread = new Thread(clientHandler);
                 thread.start();
             }
         } catch (IOException e) {
-
+            logger.error("서버 소켓을 닫는 중 오류 발생", e);
+            closeServerSocket();
         }
     }
 
@@ -34,7 +36,7 @@ public class Server {
                 serverSocket.close();
             }
         } catch (IOException e) {
-            e.printStackTrace();
+            logger.error("서버 소켓 닫기 실패", e);
         }
     }
 
