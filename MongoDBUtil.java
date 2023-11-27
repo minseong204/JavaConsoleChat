0247f5dc36cb618ef93dcd5fd805a34fdc5463a9
조민성 <128134087+minseong204@users.noreply.github.com>
2023-11-27 11:01:14 +0900

    Update MongoDBUtil.java

--- MongoDBUtil.java
+++ MongoDBUtil.java
@@ -6,13 +6,13 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class MongoDBUtil {
-    private MongoClient mongoClient;
-    private MongoDatabase database;
-    private MongoCollection<Document> chatCollection;
+    private final MongoClient mongoClient;
+    private final MongoDatabase database;
+    private final MongoCollection<Document> chatCollection;
 
     public MongoDBUtil() {
         // MongoDB 서버에 연결
-        mongoClient = MongoClients.create("mongodb+srv://계정이름:계정비밀번호@데이터베이스url");
+        mongoClient = MongoClients.create("mongodb+srv://minseong2083:ms010204324@cluster0.xhgjfwi.mongodb.net/?retryWrites=true&w=majority");
         // 데이터베이스 선택 (없으면 자동 생성)
         database = mongoClient.getDatabase("chatmanager");
         // 채팅 내역을 저장할 컬렉션 선택 (없으면 자동 생성)
@@ -35,8 +35,8 @@ public class MongoDBUtil {
         try {
             // MongoDB 컬렉션에서 메시지를 조회하고, timestamp 필드를 기준으로 내림차순 정렬
             FindIterable<Document> iterable = chatCollection.find()
-                    .sort(new Document("timestamp", 1)) // timestamp에 대해 오름차순 정렬
-                    .limit(limit); // 결과를 limit로 제한
+                    .sort(new Document("timestamp", 1)) // timestamp 대해 오름차순 정렬
+                    .limit(limit); // 결과를 limit 제한
 
             for (Document doc : iterable) {
                 messages.add(doc);
@@ -47,7 +47,7 @@ public class MongoDBUtil {
         return messages;
     }
 
-    public void deleteChatMessagesByUser(String username) {
+    public void deleteChatMessagesByUser() {
         try {
             chatCollection.deleteMany(Filters.exists("_id"));
         } catch (Exception e) {
