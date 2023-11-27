927d76bbf683a0089c473e4dd647b0c6ce76dd22
조민성 <128134087+minseong204@users.noreply.github.com>
2023-11-27 11:00:54 +0900

    Update Client.java

--- Client.java
+++ Client.java
@@ -27,7 +27,7 @@ public class Client {
     }
 
     public void register() {
-        try (Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/chatappusermanager", "계정이름", "계정비밀번호")) {
+        try (Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/chatappusermanager", "root", "ms@010204324")) {
             System.out.println("회원가입을 위해 사용자 이름을 입력해주세요:");
             String newUsername = consoleReader.readLine();
             System.out.println("비밀번호를 입력해주세요:");
@@ -51,7 +51,7 @@ public class Client {
     }
 
     public boolean login() {
-        try (Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/chatappusermanager", "계정이름", "계정비밀번호")) {
+        try (Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/chatappusermanager", "root", "ms@010204324")) {
             System.out.println("사용자 이름을 입력해주세요:");
             String username = consoleReader.readLine();
             System.out.println("비밀번호를 입력해주세요:");
@@ -96,6 +96,11 @@ public class Client {
     }
 
     public void sendMessage() {
+        if (username == null || username.trim().isEmpty()) {
+            System.out.println("사용자 이름이 설정되지 않았습니다. 로그인 또는 회원가입을 해주세요.");
+            return; // 여기서 메소드 실행을 중단합니다.
+        }
+
         try {
             bufferedWriter.write(username);
             bufferedWriter.newLine();
@@ -104,14 +109,20 @@ public class Client {
             while (socket.isConnected()) {
                 String messageToSend = consoleReader.readLine();
 
-                if (messageToSend.equals("!history")) {
-                    bufferedWriter.write(messageToSend);
-                } else if (messageToSend.equals("!Clear")) {
-                    bufferedWriter.write(messageToSend);
-                } else if (messageToSend.equals("!1")) {
-                    bufferedWriter.write(messageToSend);
-                } else {
-                    bufferedWriter.write(username + ": " + messageToSend);
+                if (messageToSend == null || messageToSend.trim().isEmpty()) {
+                    System.out.println("메시지가 비어있습니다. 다시 입력해주세요.");
+                    continue; // 빈 메시지는 무시하고 계속 진행합니다.
+                }
+
+                switch (messageToSend) {
+                    case "!history":
+                    case "!1":
+                    case "!Clear":
+                        bufferedWriter.write(messageToSend);
+                        break;
+                    default:
+                        bufferedWriter.write(username + ": " + messageToSend);
+                        break;
                 }
 
                 bufferedWriter.newLine();
@@ -121,6 +132,7 @@ public class Client {
             closeEverything(socket, bufferedReader, bufferedWriter);
         }
     }
+
 
     public void listenForMessage() {
         new Thread(() -> {
@@ -156,27 +168,38 @@ public class Client {
         }
     }
 
-    public static void main(String[] args) throws IOException {
-        System.out.println("채팅 애플리케이션에 오신 것을 환영합니다.");
-        System.out.println("1: 로그인, 2: 회원가입");
-        int choice = Integer.parseInt(consoleReader.readLine());
+    public static void main(String[] args) {
+        try {
+            System.out.println("채팅 애플리케이션에 오신 것을 환영합니다.");
+            while (true) {
+                System.out.println("1: 로그인, 2: 회원가입, 3: 종료");
+                String choiceStr = consoleReader.readLine();
+                int choice = choiceStr.isEmpty() ? 3 : Integer.parseInt(choiceStr);
 
-        Client client;
-        if (choice == 2) {
-            client = new Client(null, null);
-            client.register();
-            client = new Client(new Socket("localhost", 1234), null);
-        } else if (choice == 1) {
-            client = new Client(new Socket("localhost", 1234), null);
-            if (!client.login()) {
-                System.exit(0);
+                Client client = null;
+
+                if (choice == 2) {
+                    client = new Client(null, null);
+                    client.register();
+                    // 회원가입 후 바로 메시지 송수신을 시작하지 않고, 다시 로그인을 진행합니다.
+                    System.out.println("회원가입이 완료되었습니다. 로그인 해주세요.");
+                } else if (choice == 1) {
+                    client = new Client(new Socket("localhost", 1234), null);
+                    if (client.login()) {
+                        System.out.println("로그인 성공. 메시지를 주고받을 수 있습니다.");
+                        client.listenForMessage();
+                        client.sendMessage();
+                        break; // 로그인 성공 후 메시지 송수신을 시작하면 루프를 종료합니다.
+                    } else {
+                        System.out.println("로그인 실패: 사용자 이름 또는 비밀번호가 잘못되었습니다.");
+                    }
+                } else {
+                    System.out.println("프로그램을 종료합니다.");
+                    break;
+                }
             }
-        } else {
-            System.out.println("잘못된 선택입니다.");
-            return;
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-
-        client.listenForMessage();
-        client.sendMessage();
     }
 }
